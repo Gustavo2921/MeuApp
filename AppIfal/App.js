fd6fe3d98af53de4e40ebc3e612a6447c16d65c4
Gustavo2921 <130514398+Gustavo2921@users.noreply.github.com>
2023-07-17 08:39:56 -0300

    Update App.js

--- AppIfal/App.js
+++ AppIfal/App.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',d
+    alignItems: 'center',
     justifyContent: 'center',
   },
 });
